c53f2002d129d5dadc4bbec966d3350c27da2da8
Subhakanta Lenka <subhakantalenka@Subhakantas-MacBook-Air.local>
2025-04-11 17:51:31 +0530

    added condition for delimiter and negative number

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,15 @@
-def add(numbers)
-  return 0 if numbers.empty?
-  
-  numbers.split(',').map(&:to_i).sum
+class StringCalculator
+  def add(numbers)
+    return 0 if numbers.empty?
+    delimiter = ',|\n'
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..-1])
+      numbers = parts[1]
+    end
+    nums = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = nums.select { |num| num < 0 }
+    raise "Negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+    nums.sum
+  end
 end
